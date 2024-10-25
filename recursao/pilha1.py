d5eebe59abf2a24182efef6984c30fa809babb39
softwarekleberson <santossilvakleberson@gmail.com>
2024-10-25 10:34:27 -0300

    exemplo de pilha

---
+++ recursao/pilha1.py
@@ -0,0 +1,13 @@
+def sauda(nome):
+    print('Ola:', nome)
+    sauda2(nome)
+    print('Preparando pro adeus')
+    thau()
+
+def sauda2(nome):
+    print('Como vai:', nome)
+
+def thau():
+    print('Adeus,ate mais e obrigado pelo tempo')
+
+sauda('ana')
