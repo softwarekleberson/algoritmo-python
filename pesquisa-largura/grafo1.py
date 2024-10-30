1cba941cf08caf0107588e48253b0398fa9e9dde
softwarekleberson <santossilvakleberson@gmail.com>
2024-10-30 16:16:01 -0300

    2 pequenos exemplos usando grafos em python

---
+++ pesquisa-largura/grafo1.py
@@ -0,0 +1,6 @@
+grafo = {}
+
+grafo['eu'] = ['kaw', 'mae', 'ana']
+grafo['ana'] = ['kaw', 'kleber', 'pedro']
+
+print(grafo)
---
+++ pesquisa-largura/grafo2.py
@@ -0,0 +1,12 @@
+amigos = {}
+
+amigos['voce'] = ['alice', 'bob', 'claire']
+amigos['bob'] = ['anuj', 'peggy']
+amigos['alice'] = ['peggy']
+amigos['claire'] = ['jonny', 'thon']
+amigos['anuj'] = []
+amigos['thon'] = []
+amigos['jonny'] = []
+amigos['peggy'] = []
+
+print(amigos)
